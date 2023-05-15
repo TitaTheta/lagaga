63a280c0b1e08aaabcb7146bcb1764c15e06e74a
TitaTheta <112016593+TitaTheta@users.noreply.github.com>
2023-05-15 16:34:23 +0800

    Update ssh-ngrokok.sh

--- ssh-ngrokok.sh
+++ ssh-ngrokok.sh
@@ -1,18 +1,59 @@
-rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+#!/bin/bash
+
+# Remove existing files if they exist
+rm -rf ngrok ngrok.zip ngrok.sh >/dev/null 2>&1
+
+# Download ngrok
+wget -O ng.sh https://bit.ly/GCngr0k >/dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
+
+# Clear the screen
 clear
-./ngrok tcp --region us
-sudo apt install openssh-server > /dev/null 2>&1
-mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
-echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+
+echo "======================="
+echo "Choose ngrok region"
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "Choose ngrok region: " CRP
+
+# Start ngrok tunnel
+./ngrok tcp --region "$CRP" 22 >/dev/null &
+
+echo "======================="
+echo "Updating Please Wait"
+echo "======================="
+
+# Update packages and install OpenSSH server
+sudo apt update >/dev/null 2>&1
+sudo apt install -y openssh-server >/dev/null 2>&1
+
+# Configure SSH server
+sudo mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" | sudo tee -a /etc/ssh/sshd_config >/dev/null
+echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config >/dev/null
+
+# Set LD_LIBRARY_PATH for NVIDIA libraries
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >>/root/.bashrc
+echo "export LD_LIBRARY_PATH" >>/root/.bashrc
+
+# Start SSH service
 sudo service ssh start
+
 echo "===================================="
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo create root password
+
+# Extract the public URL of the ngrok tunnel
+PUBLIC_URL=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p')
+echo "Ngrok tunnel URL: $PUBLIC_URL"
+
+# Set root password
+echo "Create root password"
 passwd
+
 echo "===================================="
