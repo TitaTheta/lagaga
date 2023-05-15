fbc25dcb5636d017ec6f80dafbc5ab8ba54d0c55
TitaTheta <112016593+TitaTheta@users.noreply.github.com>
2023-05-15 17:04:34 +0800

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-
-# Remove existing files if they exist
 rm -rf ngrok ngrok.zip ng.sh >/dev/null 2>&1
-
 echo "======================="
 echo "Download ngrok"
 echo "======================="
-
-# Download ngrok
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>&1
 unzip ngrok.zip >/dev/null 2>&1
-
 echo "======================="
 echo "Set Ngrok Authtoken"
 echo "======================="
-
-# Prompt user for Ngrok authtoken
 read -p "Paste Ngrok Authtoken: " CRP
-
-# Set Ngrok authtoken
 ./ngrok authtoken "$CRP"
 
